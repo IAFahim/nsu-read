f07e173646c79f0d7b71e478defe3b8440da9549
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2022-10-15 03:27:06 +0600

    works

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,6 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react';
+import "../styles/globals.css"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,20 +1,35 @@
-import type {NextPage} from 'next'
-import {useState} from "react";
+import * as React from 'react';
+import {SpecialZoomLevel, Viewer, Worker} from '@react-pdf-viewer/core';
+import {defaultLayoutPlugin} from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
-const Home: NextPage = () => {
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
+const HomePage = () => {
+    const getToPageFatso = () => {
+        return defaultLayoutPlugin({
 
-    function onDocumentLoadSuccess({numPages}) {
-        setNumPages(numPages);
-    }
-    function getPDF(){
-        return "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+        });
     }
     return (
-        <>
-        </>
-    )
-}
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.js">
+            <div
+                style={{
+                    width:1000,
+                    height:"100vh"
+            }}
+            >
+                <Viewer fileUrl={"https://oeaiejocbhwsbqmweqhx.supabase.co/storage/v1/object/public/pdf/sample.pdf?t=2022-10-14T20%3A36%3A59.092Z"}
+                        plugins={[getToPageFatso()]}
+                        defaultScale={SpecialZoomLevel.PageWidth}
+                        initialPage={3}
+                        onPageChange={(e)=>{
+                            console.debug(e.doc);
+                        }}
 
-export default Home
+                />
+            </div>
+        </Worker>
+    );
+};
+
+export default HomePage;
