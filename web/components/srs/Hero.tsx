b27361903bd9b4637e2fd7fcce892fbeab219686
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2022-10-27 14:27:49 +0600

    no margin added mantine

---
+++ web/components/srs/Hero.tsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+    return (
+        <div style={{height: "100vh"}}>
+            <h1>Software Requirements Specification</h1>
+            <h2>for</h2>
+            <h1>NSU Read</h1>
+            <code>
+                Version 1.0
+                <br/>
+                Prepared by Md. Ishtiaq Ahamed Fahim
+                <br/>
+                NSU
+                <br/>
+                27/10/22
+            </code>
+        </div>
+    )
+}
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,17 @@
-import Introduction from "../components/Introduction";
+import Introduction from "../components/srs/Introduction";
+import System_Features from "../components/srs/System_Features";
+import Overall_Description from "../components/srs/Overall_Description";
+import External_Interface_Requirements from "../components/srs/External_Interface_Requirements";
+import Hero from "../components/srs/Hero";
 
-export default function Home() {
-  return (
-    <Introduction/>
-  )
+export default function SRS() {
+    return (
+        <>
+            <Hero/>
+            <Introduction/>
+            <Overall_Description/>
+            <External_Interface_Requirements/>
+            <System_Features/>
+        </>
+    )
 }
