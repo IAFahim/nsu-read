b083fe5964d9da976a0c162649c1d5fd908548cf
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2022-11-03 18:13:17 +0600

    go up

--- web/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-    return (
-        <div style={{paddingRight: 20, paddingLeft: 20}}>
-            <h1 style={{textAlign:"center"}}>Only SRS is available. <Link href={"/srs"}>SRS</Link></h1>
-        </div>
-    )
-}
