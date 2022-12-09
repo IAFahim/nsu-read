78298854e8e2f101e2dfee04cf1566514ea8cd3d
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2022-12-10 01:34:59 +0600

    You can now create group

--- web/pages/[username]/group/index.tsx
+++ web/pages/[username]/group/index.tsx
@@ -7,7 +7,16 @@ import {
     TransferList,
     TransferListData,
     TransferListItemComponent,
-    TransferListItemComponentProps, Container, Input, Flex, Button, TransferListItem, Select,
+    TransferListItemComponentProps,
+    Container,
+    Input,
+    Flex,
+    Button,
+    TransferListItem,
+    Select,
+    MultiSelect,
+    Textarea,
+    Title,
 } from '@mantine/core';
 import {Database} from "../../../utils/database.types";
 import {useSupabaseClient} from "@supabase/auth-helpers-react";
@@ -34,12 +43,35 @@ const ItemComponent: TransferListItemComponent = ({
 );
 
 interface ItemProps extends React.ComponentPropsWithoutRef<'div'> {
+    image: string;
+    label: string;
+    description: string;
+}
+
+const selectedUser = forwardRef<HTMLDivElement, ItemProps>(
+    ({image, label, description, ...others}: ItemProps, ref) => (
+        <div ref={ref} {...others}>
+            <Group noWrap>
+                <Avatar src={image}/>
+
+                <div>
+                    <Text>{label}</Text>
+                    <Text size="xs" color="dimmed">
+                        {description}
+                    </Text>
+                </div>
+            </Group>
+        </div>
+    )
+);
+
+interface ItemProps extends React.ComponentPropsWithoutRef<'div'> {
     label: string;
     description: string;
 }
 
 const SelectItem = forwardRef<HTMLDivElement, ItemProps>(
-    ({ label, description, ...others }: ItemProps, ref) => (
+    ({label, description, ...others}: ItemProps, ref) => (
         <div ref={ref} {...others}>
             <Group noWrap>
                 <div>
@@ -54,77 +86,122 @@ const SelectItem = forwardRef<HTMLDivElement, ItemProps>(
 );
 
 
-
-
 type GroupShowAll = Database['public']['Tables']['groups']['Row'];
 export default function ManageGroup() {
-    const [mainGroup, SetMainGroup] = useState([
-        {
-            label: 'Student',
-            value: 'bender',
-            image: 'https://img.icons8.com/clouds/256/000000/futurama-bender.png',
-            description: 'Fascinated with cooking, though has no sense of taste',
-        },
-
-        {
-            label: 'Student',
-            value: 'carol',
-            image: 'https://img.icons8.com/clouds/256/000000/futurama-mom.png',
-            description: 'One of the richest people on Earth',
-        }
-    ] as TransferListItem[]);
+    const [usersGroupList, SetUsersGroupList] = useState([] as TransferListItem[]);
+    const [group_members, SetGroup_members] = useState([] as TransferListItem[]);
+    const [mainGroup, SetMainGroup] = useState([] as TransferListItem[]);
     const [otherGroup, SetOtherGroup] = useState([] as TransferListItem[]);
     const [data, setData] = useState<TransferListData>([mainGroup, otherGroup]);
     const supabase = useSupabaseClient<Database>();
     const profile = useProfile(state => state.profiles);
-    const [g_name, setGName] = useState("");
-    const createGroup = async () => {
-        const data = await supabase.from("groups").insert({
-            id: profile?.id,
-            name: g_name,
 
-        });
-    };
+    const newGroupName = useRef<HTMLInputElement>("" as unknown as HTMLInputElement);
+    const [newGroupDescription, setNewGroupDescription] = useState('');
+    const mainGroupName = useRef<HTMLInputElement>("" as unknown as HTMLInputElement);
+    const otherGroupName = useRef<HTMLInputElement>("" as unknown as HTMLInputElement);
 
-
-    const getMembers = async () => {
-        const data = await supabase.from("groups").select("*");
-
-
-    };
     const lock = useRef(true);
     const router = useRouter();
     useEffect(() => {
+        async function fetchProfile() {
+            if (!router.isReady) return;
+            if (lock.current) {
+                lock.current = false;
+                const group = await supabase.from("groups").select("*").eq("created_by", profile?.username);
+                if (group.data) {
+                    SetUsersGroupList(group.data.map(function (element) {
+                        let obj = {
+                            label: element.name,
+                            value: element.name,
+                            description: element.description
 
-    }, []);
+                        }
+                        console.log(obj, element)
+                        return obj
+                    }))
+                }
+            }
+        }
+
+        fetchProfile();
+    }, [router.isReady]);
+
+    const createGroup = async () => {
+        // console.log(newGroupName.current.value, newGroupDescription)
+        const {data, error} = await supabase
+            .from('groups')
+            .insert([
+                {
+                    created_by: profile?.username,
+                    name: newGroupName.current.value,
+                    description: newGroupDescription
+                },
+            ]);
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(data);
+        }
+    }
+
+    const [value, setValue] = useState<string | null>(null);
 
     return (
         <Container>
-            <Group mb={"xs"}>
-                <Input placeholder="Name" style={{flexGrow: 1}}
-                       onChange={(e: { target: { value: SetStateAction<string>; }; }) => setGName(e.target.value)}/>
+            <Title>New</Title>
+            <Group mb={5}>
+                <Input ref={newGroupName} placeholder="Name" style={{flexGrow: 1}}/>
                 <Button onClick={createGroup}>Publish</Button>
             </Group>
+            <Textarea value={newGroupDescription}
+                      onChange={(event) => setNewGroupDescription(event.currentTarget.value)}
+                      placeholder={"Description"}></Textarea>
+            <Group>
+                <MultiSelect
+                    label="Choose User to add to this group"
+                    placeholder="comma ',' separated usernames"
+                    itemComponent={SelectItem}
+                    style={{flexGrow: 1}}
+                    data={group_members}
+                    searchable
+                    nothingFound="Nobody here"
+                    maxDropdownHeight={400}
+                    filter={(value, selected, item) =>
+                        !selected && item.label &&
+                        (item.label.toLowerCase().includes(value.toLowerCase().trim()) ||
+                            item.description.toLowerCase().includes(value.toLowerCase().trim()))
+                    }
+                />
+
+                <Button mt={"xl"} onClick={() => {
+                    console.log(value)
+                }}>Add</Button>
+            </Group>
+
+            <Title mt={"xl"}>Edit</Title>
 
             <Flex justify={"space-between"}>
                 <Select
-                    placeholder="Pick one"
+                    placeholder="Your Groups"
+                    value={value}
+                    onChange={setValue}
                     itemComponent={SelectItem}
-                    data={mainGroup}
+                    data={usersGroupList}
                     searchable
                     style={{flexGrow: 1}}
                     maxDropdownHeight={400}
                     nothingFound="Nobody here"
-                    filter={(value, item) =>
-                        // @ts-ignore
-                        item.label.toLowerCase().includes(value.toLowerCase().trim()) ||
-                        item.description.toLowerCase().includes(value.toLowerCase().trim())
-                    }
+                    filter={(value, item) => {
+                        return item.label != null && item.label.toLowerCase().includes(value.toLowerCase().trim());
+                    }}
                 />
-                <Button ml={"xs"} mr={"xl"} onClick={createGroup}>Add</Button>
+                <Button ml={"xs"} mr={"xl"} onClick={() => {
+                    console.log(value)
+                }
+                }>Add</Button>
 
-                <Input pr={"xs"} placeholder="Group to be transferred" style={{flexGrow: 1}}
-                       onChange={(e: { target: { value: SetStateAction<string>; }; }) => setGName(e.target.value)}/>
+                <Input pr={"xs"} placeholder="Group to be transferred" style={{flexGrow: 1}}/>
                 <Button>Fetch</Button>
             </Flex>
 
